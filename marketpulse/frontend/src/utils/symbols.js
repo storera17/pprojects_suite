4319d0fa3b4f98d5fdf467973d85e20b4a2717fc
Andrew Storer <andy_storer@aol.com>
2026-06-18 21:14:00 -0400

    finished symbols

--- marketpulse/frontend/src/utils/symbols.js
+++ marketpulse/frontend/src/utils/symbols.js
@@ -1,5 +1,3 @@
 export function titleCase(value) {
-    return String(value).replace(/[-_]/g, " ")
-    .replace(/\b\w/g, (m) => m
-    .toUpperCase());
+    return String(value || "").trim().toUpperCase();
 }
