1f7c6ffd525cc4333397446476796e99c0f72382
Andrew Storer <andy_storer@aol.com>
2026-07-06 14:03:44 -0400

    added features to mobile.js

---
+++ ChemPulse/backend/api/assets/mobile.js
@@ -0,0 +1,4 @@
+const $ = (id) => document.getElementById(id);
+const esc = (value) => String(value ?? "").replace(/[&<>"']/g, (char) => ({
+  "&": "&amp;", "<": "&lt;", ">": "&gt;", '"': "&quot;", "'": "&#39;"
+})[char]);
